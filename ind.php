d5679fd02772aeb6af4a277a7e05a2341f27dd20
AdamBougher <a.bougher1999@gmail.com>
2024-04-24 16:26:07 -0500

    added que

--- ind.php
+++ ind.php
@@ -72,9 +72,49 @@
     <div class="container">
         <h1 style="font-size: 2em;">Welcome to Karaoke Event Application</h1> 
         <p style="font-size: 1.5em;">Please select your role:</p>
+
         <a href="user.php" class="button">User</a>
         <a href="dj.php" class="button">DJ</a>
         <a href="admin.php" class="button">Admin</a>
+        
+        <h1>UpNext</h1>
+            <table id="songTable">
+                <tr>
+                    <th>Name</th>
+                    <th>Song</th>
+                    <th>Artist</th>
+                    <th>Version</th>
+                </tr>
+
+                
+            </table>
+
+
+
+        <h2>PLayList</h2>
+        <table id="songTable">
+                <tr>
+                    <th>Song</th>
+                    <th>Artist</th>
+                    <th>Genre</th>
+                    <th>Version</th>
+                    <th>Contributor</th>
+                    <th>Role</th>
+                </tr>
+                <?php if (!empty($songs)) : ?>
+                    <?php foreach ($songs as $song) : ?>
+                        <tr onclick="selectSong('<?php echo $song['SongID']; ?>', '<?php echo $song['SongName']; ?>', this)">
+                            <td><?php echo $song['SongName']; ?></td>
+                            <td><?php echo $song['ArtistName']; ?></td>
+                            <td><?php echo $song['GenreName']; ?></td>
+                            <td><?php echo $song['VersionName']; ?></td>
+                            <td><?php echo $song['Contributors']; ?></td>
+                            <td><?php echo $song['Roles']; ?></td>
+                        </tr>
+                    <?php endforeach; ?>
+                <?php endif; ?>
+        </table>
+
     </div>
 </body>
 
